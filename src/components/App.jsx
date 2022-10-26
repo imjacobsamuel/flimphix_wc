3eff4e5733f7be6c54fa2bc16c526d2b5c9b9247
Jacob Samuel <imjacobsamuel@gmail.com>
2022-10-26 08:49:11 +0100

    file and folder structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+import { CssBaseline } from '@mui/material';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import useStyles from './styles';
+import { Actors, MovieInformation, Movies, NavBar, Profile } from './index';
 
-const App = () => <div>App</div>;
+function App() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route exact path="/movie/:id" element={<MovieInformation />} />
+          <Route exact path="/actors/:id" element={<Actors />} />
+          <Route exact path="/" element={<Movies />} />
+          <Route exact path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Router>
+    <App />
+  </Router>,
+);
